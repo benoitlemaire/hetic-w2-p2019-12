057b946335da1fa02ea17824087a4b6e785d7080
Benoit Lemaire <benoit.lemaire@hetic.net>
2018-01-25 23:57:00 +0100

    mobile menu + animation done

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,23 @@
 export class Menu {
   constructor() {
-    let burger = document.querySelector('.navigation__burger');
+    const burger = document.querySelector('.navigation__header__burgerIcon');
     let open = false; 
+    const burgerContent = document.querySelector('.navigation__burgerContent');
+    const logo = document.querySelector('.navigation__header__logo');
+
     burger.addEventListener('click', function () {
         if (open) {
             open = false;
             burger.classList.remove('is-open');
+            burgerContent.style.transform = 'translateX(-100%)';
+            logo.style.transform = 'translateX(0)';
+            console.log('open');
         }else {
             open = true;
             burger.classList.add('is-open');
+            burgerContent.style.transform = 'translateX(0)';
+            logo.style.transform = 'translateX(-70%)';
+            console.log('close');
         }
     })
 
