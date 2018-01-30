eb24defbccb629b7ce93abc0285f5d3fa83be923
Benoit Lemaire <benoit.lemaire@hetic.net>
2018-01-31 00:18:19 +0100

    menu almost done

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,24 +1,22 @@
 export class Menu {
-  constructor() {
-  }
 
  toggleMenu () {
     let open = false; 
-    const burger = document.querySelector('.menu__burgerIcon');
+    const burger = document.querySelector('.menu__burgerIcon--open');
     const burgerContent = document.querySelector('.menu__burgerContent');
     const logo = document.querySelector('.menu__logo');
 
     burger.addEventListener('click', function () {
         if (open) {
             open = false;
-            burger.classList.remove('--open');
             burgerContent.style.transform = 'translateX(-100%)';
+            burger.style.transform = 'translateX(0)';
             logo.style.transform = 'translateX(0)';
             console.log('close');
         }else {
             open = true;
-            burger.classList.add('--open');
             burgerContent.style.transform = 'translateX(0)';
+            burger.style.transform = 'translateX(280%)';
             logo.style.transform = 'translateX(-70%)';
             console.log('open');
         }
