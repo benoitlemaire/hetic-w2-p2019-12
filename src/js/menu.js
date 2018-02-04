0154ddd3dd3516bdbadcfaa20cb00a5219147e01
Benoit Lemaire <benoit.lemaire@hetic.net>
2018-02-04 23:48:17 +0100

    mobile / tablet done, rest JS and fix bug

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,21 +8,14 @@ export class Menu {
     let openBurger = false; 
     const burger = document.querySelector('.menu__burgerIcon--open');
     const burgerContent = document.querySelector('.menu__burgerContent');
-    const logo = document.querySelector('.menu__logo');
 
     burger.addEventListener('click', function () {
         if (openBurger) {
             openBurger = false;
             burgerContent.style.transform = 'translateX(-100%)';
-            burger.style.transform = 'translateX(0)';
-            logo.style.transform = 'translateX(0)';
-            console.log('close');
         }else {
             openBurger = true;
             burgerContent.style.transform = 'translateX(0)';
-            burger.style.transform = 'translateX(280%)';
-            logo.style.transform = 'translateX(-70%)';
-            console.log('open');
         }
     })
   }
